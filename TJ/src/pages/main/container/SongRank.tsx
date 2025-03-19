db4c0e54a660d3dd24de975ad967d037134a4a16
Kangnets <kangnets88@gmail.com>
2025-03-19 13:34:59 +0900

    리펙5

--- TJ/src/pages/main/container/SongRank.tsx
+++ TJ/src/pages/main/container/SongRank.tsx
@@ -8,24 +8,26 @@ interface songRankProps {
 
 const SongRank = ({ title, name, img, award, count }: songRankProps) => {
   return (
-    <div className="flex justify-center align-middle h-12 w-[350px] bg-[#EF9659] rounded-[10px] items-center">
-      <div className="text-left">
-        <div className="font-[Pretendard] text-white text-sm font-medium text-left">
-          {count}회
+    <div className="flex justify-between  align-middle h-12 w-[350px] bg-[#EF9659] rounded-[10px] items-center">
+      <div className="flex justify-start align-middle items-center">
+        <div className="text-left pl-5">
+          <div className="font-[Pretendard] text-white text-sm font-medium text-left">
+            {count}회
+          </div>
+          <div className="font-[Pretendard] text-white text-xs font-normal">
+            {award}
+          </div>
         </div>
-        <div className="font-[Pretendard] text-white text-xs font-medium">
-          {award}
+        <div className="ml-[60px] text-left">
+          <div className="font-[Pretendard] text-white text-base font-bold">
+            {title}
+          </div>
+          <div className="-mt-1 font-[Pretendard] text-white text-sm font-normal">
+            {name}
+          </div>
         </div>
       </div>
-      <div className="ml-[60px] text-left">
-        <div className="font-[Pretendard] text-white text-base font-bold">
-          {title}
-        </div>
-        <div className="font-[Pretendard] text-white text-sm font-bold">
-          {name}
-        </div>
-      </div>
-      <div className="ml-[100px]">
+      <div className="ml-[100px] pr-5">
         <img className="w-10 h-10 rouned-[40px]" src={img}></img>
       </div>
     </div>
